2d503d0339585c0cab8c1d3c6100ddd765d84869
Gabriel Meira <gabrielcervalmeira@gmail.com>
2025-08-22 14:02:14 -0300

    feat: construcao do formulario para adicionar transacao

--- src/app/features/home/pages/create/create.ts
+++ src/app/features/home/pages/create/create.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './create.html',
   styleUrl: './create.scss'
 })
 export class Create {
 
+    form = new FormGroup({
+      title: new FormControl('', {
+        validators: [Validators.required]
+      }),
+      value: new FormControl('', {
+        validators: [Validators.required]
+      }),
+    });
 }
