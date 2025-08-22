52a817f2c814cb447a803875d7cf9cc285f6f2cf
Gabriel Meira <gabrielcervalmeira@gmail.com>
2025-08-22 14:13:13 -0300

    feat: adicionando button-toggle ao formulario de transacao

--- src/app/features/home/pages/create/create.ts
+++ src/app/features/home/pages/create/create.ts
@@ -3,17 +3,24 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { TransactionType } from '../../../../shared/transaction/enums/transaction-type';
 
 
 @Component({
   selector: 'app-create',
-  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule, MatButtonToggleModule],
   templateUrl: './create.html',
   styleUrl: './create.scss'
 })
 export class Create {
 
+  readonly transactionType = TransactionType
+
     form = new FormGroup({
+      type: new FormControl('', {
+        validators: [Validators.required]
+      }),
       title: new FormControl('', {
         validators: [Validators.required]
       }),
