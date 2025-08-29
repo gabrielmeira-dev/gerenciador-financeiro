8cb0173458e3d4cf88f497b7c083ad846367b69d
Gabriel Meira <gabrielcervalmeira@gmail.com>
2025-08-29 13:53:43 -0300

    feat: criacao de classe para estilizar o header

--- src/app/core/layout/components/header/header.ts
+++ src/app/core/layout/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
