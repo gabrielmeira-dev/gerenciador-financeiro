c10dcb5a562e182ee6facdd755ba4366647020e8
Gabriel Meira <gabrielcervalmeira@gmail.com>
2025-10-07 12:50:09 -0300

    feat: criacao de diretiva para abstrair classe css

--- src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
+++ src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card'
 import { Transaction } from '../../../../../../shared/transaction/interfaces/transaction';
 import { TransactionValue } from './components/transaction-value/transaction-value';
+import { CustomColor } from '@shared/material/buttons/directives/custom-color';
 
 @Component({
   selector: 'app-transaction-item',
-  imports: [MatCardModule, MatButtonModule, TransactionValue],
+  imports: [MatCardModule, MatButtonModule, TransactionValue, CustomColor],
   templateUrl: './transaction-item.html',
   styleUrl: './transaction-item.scss'
 })
