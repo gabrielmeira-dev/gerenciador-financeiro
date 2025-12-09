5a0453fcc2129e2276effe41fea5fd3334c0e536
Gabriel Meira <gabrielcervalmeira@gmail.com>
2025-12-09 12:55:20 -0300

    feat: alimentando grafico com dados reais

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,20 +3,27 @@ import { Balance } from './components/balance/balance';
 import { Transaction } from '@shared/transaction/interfaces/transaction';
 import { PieChart } from './components/pie-chart/pie-chart';
 import { PieChartConfig } from './components/pie-chart/pie-chart-config.interface';
+import { TransactionType } from '@shared/transaction/enums/transaction-type';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    Balance,
-    PieChart
-  ],
+  imports: [Balance, PieChart],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home { 
+export class Home {
   transactions = input.required<Transaction[]>();
 
+
+  totalIncomes = computed(() => {
+    return this.transactions().filter(item => item.type === TransactionType.INCOME).reduce((total, item) => total + item.value, 0)
+   })
+
+   totalOutcomes = computed(() => {
+     return this.transactions().filter(item => item.type === TransactionType.OUTCOME).reduce((total, item) => total + item.value, 0)
+    })
+
   chartConfig = computed<PieChartConfig>(() => {
-   return { labels: ['teste1'], dataLabels: 'Teste', data: [100] }
-  })
+    return { labels: ['Ganhos', 'Gastos'], dataLabels: 'Valor Total', data: [this.totalIncomes(), this.totalOutcomes()] };
+  });
 }
