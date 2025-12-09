d9fdd10677d648b3c886c4252eccd9d4740b6513
Gabriel Meira <gabrielcervalmeira@gmail.com>
2025-12-09 12:43:02 -0300

    feat: criacao de componente de grafico de gastos e ganhos

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,13 @@
 import { Component, input } from '@angular/core';
 import { Balance } from './components/balance/balance';
 import { Transaction } from '@shared/transaction/interfaces/transaction';
+import { PieChart } from './components/pie-chart/pie-chart';
 
 @Component({
   selector: 'app-home',
   imports: [
     Balance,
+    PieChart
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
